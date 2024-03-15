223f5bd63780d3129e06672c35c9126d11f5895f
BenjaminDLC <benjamin.delima-cruz@epitech.eu>
2024-03-15 09:30:59 +0100

    FIX: deleted program code

--- donation/Program.java
+++ donation/Program.java
@@ -9,38 +9,5 @@ public class Programa
 {
     public static void main(String[] args){
         Teclado teclado = new Teclado();
-        
-        Doacao[] doacoes = new Doacao[10000];
-        int indice = 0;
-        int continuar = 2;
-        
-        do{
-            Doacao doacao = new Doacao();
-            
-            System.out.println("Informe o nome do doador:");
-            doacao.nomeDoador = teclado.leString();
-            
-            System.out.println("Informe o valor da doação:");
-            doacao.valor = teclado.leDouble();
-            
-            doacoes[indice] = doacao;
-            indice = indice + 1;
-            
-            System.out.println("Deseja informar uma nova doação?(1 para sim e 2 para não):");
-            continuar = teclado.leInt();
-            
-        }while(continuar == 1);
-        
-        int quantidadeDoacoes = indice;
-        double somaDoacoes = 0;
-        indice = 0;
-        
-        while(indice < quantidadeDoacoes){
-            somaDoacoes = somaDoacoes + doacoes[indice].valor;
-            indice = indice + 1;
-        }
-        
-        System.out.println("Valor total das doações: " + somaDoacoes);
-        System.out.println("Quantidade de doações: " + quantidadeDoacoes);
     }
 }
--- variables/Program.java
+++ variables/Program.java
@@ -9,34 +9,5 @@ public class Programa
 {
     public static void main(String[] args){
         Teclado teclado = new Teclado();
-        
-        double[]idades = new double[10000];
-        int contagem = 0;
-        double ultimaIdadeDigitada = 0;
-        
-        System.out.println("Informe valores de idades de pessoas. Informe um vaor negativo para sair: ");
-        
-        do{
-            ultimaIdadeDigitada = teclado.leDouble();
-            if(ultimaIdadeDigitada >= 0){
-                idades[contagem] = ultimaIdadeDigitada;
-                contagem = contagem + 1;
-            }
-        }while(ultimaIdadeDigitada >= 0);
-        
-        int quantidadeIdadesInformadas = contagem;
-        double somaIdades = 0;
-        contagem = 0;
-        
-        while(contagem < quantidadeIdadesInformadas){
-            somaIdades = somaIdades + idades[contagem];
-            contagem = contagem + 1;
-        }
-        
-        double media = ultimaIdadeDigitada / quantidadeIdadesInformadas;
-        
-        System.out.println("Soma das idades informadas: " + somaIdades);
-        System.out.println("A média das idades é: " + media);
-        
     }
 }
